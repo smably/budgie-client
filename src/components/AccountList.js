f8a49c74bd985380f1de22b8d539b32804f0efac
Sylvan Mably <smably@ebay.com>
2015-07-19 00:05:43 -0400

    Move account modals up a level to AccountsView

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -2,25 +2,9 @@
 
 var React = require('react/addons');
 
-var Button = require('react-bootstrap').Button;
-
 var Account = require('components/Account');
-var AccountModal = require('components/AccountModal');
-var DeleteAccountModal = require('components/DeleteAccountModal');
 
 var AccountList = React.createClass({
-  getInitialState: function() {
-    return { activeAccount: null };
-  },
-
-  updateActiveAccount: function(newActiveAccount) {
-    this.setState({ activeAccount: newActiveAccount });
-  },
-
-  resetActiveAccount: function() {
-    this.setState({ activeAccount: null });
-  },
-
   renderAccountRows: function() {
     var accountRows;
 
@@ -36,66 +20,40 @@ var AccountList = React.createClass({
           <Account
             key={account.id}
             data={account}
-            editCallback={ function() { this.openEditAccountModal(account); }.bind(this) }
-            deleteCallback={ function() { this.openDeleteAccountModal(account); }.bind(this) }/>
+            editCallback={ function() { this.props.editCallback(account); }.bind(this) }
+            deleteCallback={ function() { this.props.deleteCallback(account); }.bind(this) }
+          />
         );
       }.bind(this)).toArray();
     } else {
-      accountRows = [
-        <tr key="noAccountsFound">
+      accountRows = (
+        <tr>
           <td colSpan="8">No accounts found!</td>
         </tr>
-      ];
+      );
     }
 
     return accountRows;
-  },
-
-  openAddAccountModal: function() {
-    if (this.state.activeAccount) {
-      this.resetActiveAccount();
-    }
-
-    this.refs.accountModal.open();
-  },
-
-  openEditAccountModal: function(account) {
-    this.updateActiveAccount(account);
-    this.refs.accountModal.open();
-  },
-
-  openDeleteAccountModal: function(account) {
-    this.updateActiveAccount(account);
-    this.refs.deleteAccountModal.open();
   },
 
   render: function() {
     var accountRows = this.renderAccountRows();
 
     return (
-      <div>
-        <Button bsStyle='primary' onClick={this.openAddAccountModal}>
-          <span className="glyphicon glyphicon-plus"></span> New Account
-        </Button>
-
-        <table className='main'>
-          <tbody>
-            <tr>
-              <th>Label</th>
-              <th>Source</th>
-              <th>Destination</th>
-              <th>Primary</th>
-              <th>Institution Name</th>
-              <th>Account Info</th>
-              <th></th>
-            </tr>
-            {accountRows}
-          </tbody>
-        </table>
-
-        <AccountModal ref='accountModal' account={this.state.activeAccount}/>
-        <DeleteAccountModal ref='deleteAccountModal' account={this.state.activeAccount}/>
-      </div>
+      <table className='main'>
+        <tbody>
+          <tr>
+            <th>Label</th>
+            <th>Source</th>
+            <th>Destination</th>
+            <th>Primary</th>
+            <th>Institution Name</th>
+            <th>Account Info</th>
+            <th></th>
+          </tr>
+          {accountRows}
+        </tbody>
+      </table>
     );
   }
 });
--- src/components/AccountsView.js
+++ src/components/AccountsView.js
@@ -3,6 +3,7 @@
 var React = require('react/addons');
 var Reflux = require('reflux');
 var Router = require('react-router');
+var Button = require('react-bootstrap').Button;
 var Link = Router.Link;
 
 var AccountActions = require('actions/AccountActions');
@@ -10,6 +11,8 @@ var AccountActions = require('actions/AccountActions');
 var AccountStore = require('stores/AccountStore');
 
 var AccountList = require('components/AccountList');
+var AccountModal = require('components/AccountModal');
+var DeleteAccountModal = require('components/DeleteAccountModal');
 
 var AccountsView = React.createClass({
   mixins: [Reflux.connect(AccountStore, "accounts")],
@@ -18,12 +21,52 @@ var AccountsView = React.createClass({
     AccountActions.load();
   },
 
+  getInitialState: function() {
+    return { activeAccount: null };
+  },
+
+  updateActiveAccount: function(newActiveAccount) {
+    this.setState({ activeAccount: newActiveAccount });
+  },
+
+  resetActiveAccount: function() {
+    this.setState({ activeAccount: null });
+  },
+
+  openAddAccountModal: function() {
+    this.resetActiveAccount();
+    this.refs.accountModal.open();
+  },
+
+  openEditAccountModal: function(account) {
+    this.updateActiveAccount(account);
+    this.refs.accountModal.open();
+  },
+
+  openDeleteAccountModal: function(account) {
+    this.updateActiveAccount(account);
+    this.refs.deleteAccountModal.open();
+  },
+
   render: function() {
     return (
       <div>
         <h3>Accounts</h3>
-        <AccountList accounts={this.state.accounts} />
+
+        <Button bsStyle='primary' onClick={this.openAddAccountModal}>
+          <span className="glyphicon glyphicon-plus"></span> New Account
+        </Button>
+
+        <AccountList
+          accounts={this.state.accounts}
+          editCallback={this.openEditAccountModal}
+          deleteCallback={this.openDeleteAccountModal}
+        />
+
         <Link to="transactions">All Transactions</Link>
+
+        <AccountModal ref='accountModal' account={this.state.activeAccount}/>
+        <DeleteAccountModal ref='deleteAccountModal' account={this.state.activeAccount}/>
       </div>
     );
   }
